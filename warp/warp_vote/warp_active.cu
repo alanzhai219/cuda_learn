e21cc153dac6f94349846e924bca5f4fe1e6e32a
zhaixiuchuan <zhaixiuchuan@hotmail.com>
2021-08-17 17:43:28 +0800

    fix warp active 2nd

--- warp/warp_vote/warp_active.cu
+++ warp/warp_vote/warp_active.cu
@@ -8,26 +8,17 @@
 __global__ void vote_active() {
   int tid = blockDim.x * blockIdx.x + threadIdx.x;
   int fullmask = __activemask();
-  printf("fullmask -- %x\n", fullmask);
+  printf("tid = %d, fullmask = %x\n", tid, fullmask);
   if (tid%2 == 0) {
     int mask = __activemask();
-    printf("mask -- %x\n", mask);
+    printf("tid = %d, mask = %x\n", tid, mask);
   }
 }
 
 int main(int argc, char* argv[]) {
 
-    std::cout << "<<<1,64>>>\n";
-    dim3 gridSize0(1);
-    dim3 blockSize0(64);
-    vote_active<<<gridSize0, blockSize0>>>();
-    cudaDeviceSynchronize();
-    
-    std::cout << "<<<1,48>>>\n";
     dim3 gridSize1(1);
     dim3 blockSize1(48);
-    vote_active<<<gridSize1, blockSize1>>>();
-    cudaDeviceSynchronize();
 
     std::cout << "branch <<<1,48>>>\n";
     vote_active<<<gridSize1, blockSize1>>>();
