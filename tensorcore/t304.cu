e2b33e3e86f24f71898845ca4672e9259b9ed288
zhaixiuchuan <zhaixiuchuan@hotmail.com>
2021-12-01 18:05:38 +0800

    analysis wmma

--- tensorcore/t304.cu
+++ tensorcore/t304.cu
@@ -23,24 +23,39 @@ __global__ void wmma_ker(half *a, half *b, float *c) {
    wmma::store_matrix_sync(c, c_frag, 16, wmma::mem_row_major);
 }
 
+template <typename T>
+void dump_matrix(T *data_ptr, size_t M, size_t N) {
+  for (int i=0; i<M; ++i) {
+    for (int j=0; j<N; ++j) {
+      std::cout << static_cast<float>(data_ptr[i*M+j]) << ", ";
+    }
+    std::cout << "\n";
+  }
+}
 int main(){
 
   half *d_a, *h_a, *d_b, *h_b;
   float *d_c, *h_c;
-  h_c = new float[16*16];
+  // type of a and b is half
   h_b = new half[16*16];
   h_a = new half[16*16];
+
+  // type of c is float.
+  h_c = new float[16*16];
+
   cudaMalloc(&d_a, 16*16*sizeof(half));
   cudaMalloc(&d_b, 16*16*sizeof(half));
   cudaMalloc(&d_c, 16*16*sizeof(float));
   for (int i = 0; i < 16*16; i++) {
-    h_a[i] = 1.0f;
-    h_b[i] = 1.0f;}
+    h_a[i] = 0.0f + i;
+    h_b[i] = 0.0f + i;
+  }
+  dump_matrix(h_a, 16, 16);
+  dump_matrix(h_b, 16, 16);
   cudaMemcpy(d_a, h_a, 16*16*sizeof(half), cudaMemcpyHostToDevice);
   cudaMemcpy(d_b, h_b, 16*16*sizeof(half), cudaMemcpyHostToDevice);
   wmma_ker<<<1,32>>>(d_a, d_b, d_c);
   cudaMemcpy(h_c, d_c, 16*16*sizeof(float), cudaMemcpyDeviceToHost);
-  for (int i = 0; i < 16*16; i++) 
-    std::cout << h_c[i] << ",";
-  std::cout << std::endl;
+  dump_matrix(h_c, 16, 16);
+  return 0;
 }
